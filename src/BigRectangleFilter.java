5a691d652b7560eba7c2fb767ff1a59816738fb0
Alexander Wright <Alexwright45177@gmail.com>
2025-02-17 19:33:12 -0500

    Update BigRectangleFilter.java

--- src/BigRectangleFilter.java
+++ src/BigRectangleFilter.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.awt.*;
 
-public class BigRectangleFilter {
+public class BigRectangleFilter implements Filter {
+    @Override
+    public boolean accept(Object x) {
+        if (x instanceof Rectangle) {
+            Rectangle r = (Rectangle) x;
+            return 2 * (r.width + r.height) > 10;
+        }
+        return false;
+    }
 }
