584f3fb0737099014b4198dac4054b770d7998df
Alexander Wright <Alexwright45177@gmail.com>
2025-02-17 19:33:44 -0500

    Update ShortLister.java

--- src/ShortLister.java
+++ src/ShortLister.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+//main
+
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
 
 public class ShortLister {
+    public static void main(String[] args) {
+        JFileChooser fileChooser = new JFileChooser();
+        if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            ShortWordFilter filter = new ShortWordFilter();
+
+            try (Scanner scanner = new Scanner(file)) {
+                while (scanner.hasNext()) {
+                    String word = scanner.next();
+                    if (filter.accept(word)) {
+                        System.out.println(word);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Error reading file: " + e.getMessage());
+            }
+        }
+    }
 }
